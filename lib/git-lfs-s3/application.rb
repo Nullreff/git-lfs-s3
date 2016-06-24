74ba0a1159182fc74d5c81fd30a9ebcaafade65c
Ryan Mendivil <ryanme@unity3d.com>
2016-06-24 10:41:32 -0700

    Monkey patch aws-sdk to support additional query parameters

--- lib/git-lfs-s3/application.rb
+++ lib/git-lfs-s3/application.rb
@@ -72,53 +72,7 @@ module GitLfsS3
 
       # For more information about authentication in LFS,
       # read https://github.com/github/git-lfs/issues/960 
-
-      object_link = object_data(project_guid, params[:oid]).public_url
-      uri = URI.parse(object_link)
-      now = Time.now.utc
-      now_datetime = now.strftime('%Y%m%dT%H%M%SZ')
-      now_date = now.strftime('%Y%m%d')
-      method = 'GET'
-      expires = (5 * 60).to_s # 5 min
-      payload_hash = (Digest::SHA2.new << '').to_s
-      algorithm = 'AWS4-HMAC-SHA256'
-      service = 's3'
-      cred_scope = "#{now_date}/#{aws_region}/#{service}/aws4_request"
-      credential = aws_access_key_id + '/' + cred_scope
-
-      headers = {
-        'Host' => uri.host,
-      }
-      signed_headers = headers.keys.map(&:downcase).join(';')
-      header_list = headers.map{|k,v| "#{k.downcase}:#{v.downcase.strip.gsub(/  /, ' ')}"}.sort.join("\n")
-
-      query = {
-        'token' => '1',
-        'X-Amz-Algorithm' => algorithm,
-        'X-Amz-Credential' => credential,
-        'X-Amz-Date' => now_datetime,
-        'X-Amz-Expires' => expires,
-        'X-Amz-SignedHeaders' => signed_headers,
-      }
-      uri.query = URI.encode_www_form(query.sort_by{|k,v| k})
-
-      # Step 1: Construct Canonical Request
-      canonical_request = [method, uri.path, uri.query, header_list + "\n", signed_headers, payload_hash].join("\n")
-      logger.debug "Canonical Request: \n#{canonical_request}"
-
-      # Step 2: String to Sign
-      request_hash = (Digest::SHA2.new << canonical_request).to_s
-      string_to_sign = [algorithm, now_datetime, cred_scope, request_hash].join("\n")
-      logger.debug "String To Sign: \n#{string_to_sign}"
-
-      # Step 3: Calculate Signature
-      signing_key = hmac(hmac(hmac(hmac('AWS4' + aws_secret_access_key, now_date), aws_region), service), 'aws4_request')
-      signature = hmac(signing_key, string_to_sign, true)
-      logger.debug "Signature: \n#{signature}"
-
-      # Step 4: Add Signature to Request
-      query['X-Amz-Signature'] = signature
-      uri.query = URI.encode_www_form(query.sort_by{|k,v| k})
+      request = object_data(project_guid, params[:oid]).presigned_url_with_query(:get)
 
       status 200
       resp = {
@@ -130,11 +84,10 @@ module GitLfsS3
           },
           'download' => {
             # TODO: cloudfront support
-            'href' => uri.to_s,
+            'href' => request.to_s
           }
         }
       }
-      logger.debug "Response: \n#{JSON.pretty_generate(resp)}"
 
       body MultiJson.dump(resp)
     end
--- lib/git-lfs-s3/aws.rb
+++ lib/git-lfs-s3/aws.rb
@@ -34,3 +34,97 @@ module GitLfsS3
     end
   end
 end
+
+module Aws
+  module S3
+    class Object
+      def presigned_url_with_query(http_method, params = {})
+        presigner = Aws::S3::Presigner.new(client: client)
+        presigner.presigned_url_with_query("#{http_method.downcase}_object", params.merge(
+          bucket: bucket_name,
+          key: key,
+        ))
+      end
+    end
+
+    class Presigner
+      def presigned_url_with_query(method, params = {})
+        if params[:key].nil? or params[:key] == ''
+          raise ArgumentError, ":key must not be blank"
+        end
+        virtual_host = !!params.delete(:virtual_host)
+        scheme = http_scheme(params, virtual_host)
+
+        req = @client.build_request(method, params)
+
+        use_bucket_as_hostname(req) if virtual_host
+        sign_request(req, expires_in(params), scheme)
+        req.send_request.data
+      end
+
+private
+
+      def http_scheme(params, virtual_host)
+        if params.delete(:secure) == false || virtual_host
+          'http'
+        else
+          @client.config.endpoint.scheme
+        end
+      end
+
+      def expires_in(params)
+        if expires_in = params.delete(:expires_in)
+          if expires_in > ONE_WEEK
+            msg = "expires_in value of #{expires_in} exceeds one-week maximum"
+            raise ArgumentError, msg
+          end
+          expires_in
+        else
+          FIFTEEN_MINUTES
+        end
+      end
+
+      def use_bucket_as_hostname(req)
+        req.handlers.remove(Plugins::S3BucketDns::Handler)
+        req.handle do |context|
+          uri = context.http_request.endpoint
+          uri.host = context.params[:bucket]
+          uri.path.sub!("/#{context.params[:bucket]}", '')
+          uri.scheme = 'http'
+          uri.port = 80
+          @handler.call(context)
+        end
+      end
+
+      def sign_request(req, expires_in, scheme)
+        req.handlers.remove(Plugins::S3RequestSigner::SigningHandler)
+        req.handlers.remove(Seahorse::Client::Plugins::ContentLength::Handler)
+        req.handle(step: :send) do |context|
+          if scheme != context.http_request.endpoint.scheme
+            endpoint = context.http_request.endpoint.dup
+            endpoint.scheme = scheme
+            endpoint.port = (scheme == 'http' ? 80 : 443)
+            context.http_request.endpoint = URI.parse(endpoint.to_s)
+          end
+
+          uri = context.http_request.endpoint
+          query = Hash[URI.decode_www_form(uri.query || '')]
+          query['token'] = '1'
+          uri.query = URI.encode_www_form(query)
+
+          signer = Signers::V4.new(
+            context.config.credentials, 's3',
+            context.config.region
+          )
+          url = signer.presigned_url(
+            context.http_request,
+            expires_in: expires_in,
+            body_digest: "UNSIGNED-PAYLOAD"
+          )
+          Seahorse::Client::Response.new(context: context, data: url)
+        end
+      end
+
+    end
+  end
+end
