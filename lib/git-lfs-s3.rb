6a15445484b42b1ab8896d0e38cf81fe8af502c7
Ryan Mendivil <ryanme@unity3d.com>
2016-08-02 17:22:40 -0700

    Add secure token authentication

--- lib/git-lfs-s3.rb
+++ lib/git-lfs-s3.rb
@@ -3,5 +3,4 @@ require 'aws-sdk'
 require 'multi_json'
 
 require "git-lfs-s3/aws"
-require "git-lfs-s3/services/upload"
 require "git-lfs-s3/application"
--- lib/git-lfs-s3/application.rb
+++ lib/git-lfs-s3/application.rb
@@ -1,24 +1,12 @@
-require 'digest/sha1'
+require 'base64'
+require 'openssl'
+require 'time'
 
 module GitLfsS3
   class Application < Sinatra::Application
     include AwsHelpers
 
-    class << self
-      attr_reader :auth_callback
-
-      def on_authenticate(&block)
-        @auth_callback = block
-      end
-
-      def authentication_enabled?
-        !auth_callback.nil?
-      end
-
-      def perform_authentication(username, password)
-        auth_callback.call(username, password)
-      end
-    end
+    EXPIRATION = (60 * 15) # Fifteen minutes
 
     configure do
       disable :sessions
@@ -30,35 +18,62 @@ module GitLfsS3
         settings.logger
       end
 
+      def digest
+        OpenSSL::Digest.new('sha256')
+      end
+
       def project
         GitLfsS3::Application.settings.project_selector(request)
       end
 
-      #TODO: Generate an actual secure token
-      def generate_token
-        Digest::SHA1.hexdigest project
+      def secret
+        GitLfsS3::Application.settings.token_secret
       end
 
       def verify_link 
         protocol = GitLfsS3::Application.settings.server_ssl ? 'https' : 'http'
         server_path = GitLfsS3::Application.settings.server_path.gsub(':project', project)
         host = request.host_with_port
-        "#{protocol}://#{host}#{File.join(server_path, 'verify')}?token=#{generate_token}"
+        "#{protocol}://#{host}#{File.join(server_path, 'verify')}?token=1"
       end
 
       def verify_header
-        {Authorization: "RemoteAuth #{generate_token}"}
+        expires = (Time.now.utc + EXPIRATION).iso8601
+        message = {project: project, expires: expires}
+        message[:verify] = OpenSSL::HMAC.hexdigest(digest, secret, message.to_json)
+        token = Base64.strict_encode64(MultiJson.dump(message))
+        {Authorization: "RemoteAuth #{token}"}
       end
     end
 
-    def authorized?
-      request.env['HTTP_AUTHORIZATION'] == "RemoteAuth #{generate_token}"
+    def check_authorizization
+      header = request.env['HTTP_AUTHORIZATION']
+      return false, "Missing auth header" unless header
+
+      token = header[/^RemoteAuth (.+)$/, 1]
+      return false, "Missing auth token" unless token
+
+      message = MultiJson.load(Base64.decode64(token)) rescue nil
+      return false, "Error decoding auth token" unless message
+
+      expires = DateTime.parse(message['expires']) rescue nil
+      return false, "No auth token expriation" unless message
+
+      found_sig = message.delete('verify')
+      expected_sig = OpenSSL::HMAC.hexdigest(digest, secret, message.to_json)
+
+      return false, "Mismatching signatures\nReceived: '#{found_sig}'\nExpected: '#{expected_sig}'" unless found_sig == expected_sig
+      return false, "Mismatching projects" unless message['project'] == project
+      return false, "Auth token has expired" unless expires > Time.now.utc
+
+      return true
     end
 
     def protected!
-      unless authorized?
+      result, message = check_authorizization
+      unless result
         response['WWW-Authenticate'] = %(RemoteAuth realm="Restricted Area")
-        throw(:halt, [401, 'Invalid authorization token'])
+        throw(:halt, [401, message])
       end
     end
 
@@ -123,7 +138,7 @@ module GitLfsS3
           },
           'download' => {
             # TODO: cloudfront support
-            'href' => object_data(project, params[:oid]).presigned_url_with_token(:get)
+            'href' => object.presigned_url_with_token(:get)
           }
         }
       }
@@ -132,12 +147,19 @@ module GitLfsS3
     end
 
     post "/objects", provides: 'application/vnd.git-lfs+json' do
-      logger.debug headers.inspect
-      service = UploadService.service_for(project, request)
-      logger.debug service.response
+      data = MultiJson.load request.body.tap { |d| d.rewind }.read
+      object = object_data(project, data['oid'])
 
-      status service.status
-      body MultiJson.dump(service.response)
+      links = if object.exists? && object.size == data['size']
+        status 200
+        {download: {href: object.presigned_url_with_token(:get)}}
+      else
+        status 202
+        {upload: {href: object.presigned_url_with_token(:put)},
+         verify: {href: verify_link, header: verify_header}}
+      end
+
+      body MultiJson.dump({_links: links})
     end
 
     post '/verify', provides: 'application/vnd.git-lfs+json' do
